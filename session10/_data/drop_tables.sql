ce66f8222599b571a802f0463668a0102d85f006
ilijalazarevic <ilija.lazarevic@databrut.com>
2023-03-26 22:35:48 +0100

    Add initial versions of SQL files and notebook

---
+++ session10/_data/drop_tables.sql
@@ -0,0 +1,16 @@
+USE nycflights;
+
+-- drop table airlines
+DROP TABLE IF EXISTS airlines;
+
+-- drop table flights
+DROP TABLE IF EXISTS flights;
+
+-- drop table planes
+DROP TABLE IF EXISTS planes;
+
+-- drop table weather
+DROP TABLE IF EXISTS weather;
+
+-- drop table airports
+DROP TABLE IF EXISTS airports;
