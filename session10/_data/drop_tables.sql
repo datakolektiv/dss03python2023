a13ed9720da3aa06b135917e9ae4af36002dc775
ilijalazarevic <ilija@resights.dk>
2023-04-02 15:18:47 +0200

    Update sql script

--- session10/_data/drop_tables.sql
+++ session10/_data/drop_tables.sql
@@ -11,7 +11,8 @@ DROP TABLE IF EXISTS flights_i;
 DROP INDEX IF EXISTS flights_i_index_1 ON flights_i;
 DROP INDEX IF EXISTS flights_i_index_2 ON flights_i;
 DROP INDEX IF EXISTS flights_i_index_3 ON flights_i;
-
+DROP INDEX IF EXISTS flights_i_index_4 ON flights_i;
+DROP INDEX IF EXISTS flights_i_index_5 ON flights_i;
 
 -- drop table planes
 DROP TABLE IF EXISTS planes;
